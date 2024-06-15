5713228dd11a1fa914ed30a7b8bdad45eee0a680
KIM-JANG-BAE <100831121+KIM-JANG-BAE@users.noreply.github.com>
2024-06-15 22:48:02 +0900

    Update Saveobj.py

---
+++ Server/Saveobj.py
@@ -0,0 +1,24 @@
+import asyncio
+import websockets
+
+async def file_receiver(websocket):
+    print("Client connected")
+
+
+    # Save the file
+    with open('/home/jeonjunsu/obj/mesh1.obj', 'wb') as f:
+        while True:
+            try:
+                file_content = await websocket.recv()
+                if file_content == b'EOF':
+                    break
+                f.write(file_content)
+            except websocket.exceptions.ConnectionClosed:
+                pass
+
+    print(f"File received and saved")
+
+start_server = websockets.serve(file_receiver, '203.255.57.136', 5260)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
